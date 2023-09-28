0fb922ca149108bffc510b0fcae3e78a3ed63903
Diegodelias <diegodelias@gmail.com>
2023-09-28 19:32:48 -0300

    ejemplo jpa

---
+++ src/main/java/com/example/pruebaJPA/service/IPersonaService.java
@@ -0,0 +1,9 @@
+package com.example.pruebaJPA.service;
+
+import com.example.pruebaJPA.repository.IPersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public interface IPersonaService {
+
+
+}
